3ad156373513aec285c3fca4933ad611ef89bf5a
fila2021 <timtey2015@gmail.com>
2025-11-28 01:26:41 +0000

    Create template for about page

--- gezana_app/urls.py
+++ gezana_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("book/", views.make_booking, name="make_booking"),
     path("booking-success/", views.booking_success, name="booking_success"),
     path("cancel-booking/", views.cancel_booking, name="cancel_booking"),
+path("about/", views.about, name="about"),
 
 ]
 
