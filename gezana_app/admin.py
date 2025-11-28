8669c082e22c0389d7bc043a166a4bd14abf78d6
fila2021 <timtey2015@gmail.com>
2025-11-28 00:46:56 +0000

    Create Admin Registration for Booking and Table

--- gezana_app/admin.py
+++ gezana_app/admin.py
@@ -6,3 +6,16 @@ class MenuItemAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "price", "is_vegetarian")
     list_filter = ("category", "is_vegetarian")
     search_fields = ("name", "description")
+
+from .models import Table, Booking
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("table_number", "capacity")
+    list_filter = ("capacity",)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("name", "date", "time", "guests", "table")
+    list_filter = ("date", "time", "table")
