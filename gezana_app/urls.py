f02259d7a070ece69271760c540443d560872a72
fila2021 <timtey2015@gmail.com>
2025-11-28 00:50:58 +0000

    Add URLs for Booking

--- gezana_app/urls.py
+++ gezana_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'gezana_app'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('menu/', views.menu_list, name='menu_list'),
-    path('menu/<int:pk>/', views.menu_detail, name='menu_detail'),
+    path("", views.home, name="home"),
+    path("menu/", views.menu_list, name="menu_list"),
+    path("menu/<int:pk>/", views.menu_detail, name="menu_detail"),
+    path("book/", views.make_booking, name="make_booking"),
+    path("booking-success/", views.booking_success, name="booking_success"),
 ]
+
